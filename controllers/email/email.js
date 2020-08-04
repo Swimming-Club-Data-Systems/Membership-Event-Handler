06a4ea16b2b92fdcf18e60d6282c81d49add52b5
Chris Heppell <clheppell@icloud.com>
2020-08-04 22:44:06 +0100

    Styled emails in production

--- controllers/email/email.js
+++ controllers/email/email.js
@@ -5,7 +5,7 @@
 const htmlToText = require('html-to-text');
 const sgMail = require('@sendgrid/mail');
 const time = require('moment-timezone');
-
+const escape = require('escape-html');
 
 class Email {
 
@@ -26,7 +26,7 @@ class Email {
     } else {
       this.senderName = 'SCDS';
       this.senderEmail = 'noreply@myswimmingclub.uk';
-    }    
+    }
     this.subject = subject;
     this.htmlContent = content;
     this.textContent = htmlToText.fromString(content);
@@ -34,14 +34,82 @@ class Email {
 
   getHtml() {
 
+    let fontStack = '"Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"';
+    if (this.org.isCLS()) {
+      fontStack = '"Open Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"';
+    }
+
+    let head = `
+    <!DOCTYPE html>
+    <html lang="en-gb">
+    <head>
+      <meta charset="utf-8">`;
+    if (this.org.isCLS()) {
+      head += `<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet" type="text/css">`;
+    } else {
+      head += `<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet" type="text/css">`;
+    }
+    head += `
+      <style type="text/css">
+        html, body {
+          font-family: ${fontStack};
+          font-size: 16px;
+          background: #e3eef6;
+        }
+        p, h1, h2, h3, h4, h5, h6, ul, ol, img, .table, blockquote {
+          margin: 0 0 16px 0;
+          font-family:  ${fontStack};
+        }
+        .small {
+          font-size: 11px;
+          color: #868e96;
+          margin-bottom: 11px;
+        }
+        .text-center {
+          text-align: center;
+        }
+        .bottom {
+          margin: 16px 0 0 0;
+        }
+        cell {
+          display: table;
+          background: #eee;
+          padding: 1rem;
+          margin 0 0 1rem 0;
+          width: 100%;
+        }
+      </style>
+    </head>
+    <body>
+    <div style="background:#e3eef6;">
+      <table style="width:100%;border:0px;text-align:left;padding:10px 0px 10px 0px;background:#e3eef6;"><tr><td align="center">
+        <table style="width:100%;max-width:700px;border:0px;text-align:center;background:#ffffff;padding:10px 10px 0px 10px;"><tr><td>`;
+    if (this.org.isCLS()) {
+      head += `<img src="${escape(process.env.PUBLIC_URL + "public/img/notify/NotifyLogo.png")}"
+        style="width:300px;max-width:100%;" srcset="${escape(process.env.PUBLIC_URL + "public/img/notify/NotifyLogo@2x.png")} 2x, ${escape(process.env.PUBLIC_URL + "public/img/notify/NotifyLogo@3x.png")} 3x" alt="${escape(this.org.getName())} Logo">`;
+      // } else if (isset(app()->tenant) && $logos = app()->tenant->getKey('LOGO_DIR')) {
+      //   $head .= "<img src=\"" . autoUrl($logos . 'logo-150.png') . "\" srcset=\"" .
+      //   autoUrl($logos . 'logo-150@2x.png') . " 2x, " .
+      //   autoUrl($logos . 'logo-150@3x.png') . " 3x\" style=\"max-width:100%;max-height:150px;\" alt=\"" . htmlspecialchars(app()->tenant->getKey('CLUB_NAME')) . " Logo\">";
+    } else if (this.org) {
+      head += escape(this.org.getName());
+    } else {
+      head += 'SCDS Membership MT';
+    }
+    head += `</td></tr></table>
+        <table style="width:100%;max-width:700px;border:0px;text-align:left;background:#ffffff;padding:0px 10px;"><tr><td>`;
+
     let address = [];
     if (this.org.getKey('CLUB_ADDRESS')) {
       try {
         let address = JSON.parse(this.org.getKey('CLUB_ADDRESS'))
-      } catch (err) {}
+      } catch (err) { }
     }
 
-    let footer = '<address>'
+    let footer = `</td></tr></table>
+    <table style="width:100%;max-width:700px;border:0px;background:#f8fcff;padding:0px 10px;"><tr><td>
+    <div class="bottom text-center">
+    <address>`;
     footer += this.org.getName() + '<br>';
     address.forEach(line => {
       footer += line + '<br>';
@@ -58,7 +126,14 @@ class Email {
 
     footer += '<p>Content copyright ' + time.tz('Europe/London').format('Y') + ' ' + this.org.getName() + ', Design copyright ' + time.tz('Europe/London').format('Y') + ' SCDS.</p>';
 
-    return this.htmlContent + footer;
+    footer += `</div>
+    </table>
+  </table>
+  </div>
+  </body>
+  </html>`;
+
+    return head + this.htmlContent + footer;
   }
 
   getText() {
@@ -66,7 +141,7 @@ class Email {
     if (this.org.getKey('CLUB_ADDRESS')) {
       try {
         let address = JSON.parse(this.org.getKey('CLUB_ADDRESS'))
-      } catch (err) {}
+      } catch (err) { }
     }
 
     let footer = '\r\n\r\n' + this.org.getName() + '\r\n';
@@ -109,7 +184,7 @@ class Email {
         await sgMail.send(msg);
       } catch (error) {
         console.error(error);
-    
+
         if (error.response) {
           console.error(error.response.body)
         }
