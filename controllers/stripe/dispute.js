ab8c80928ca92e2f88452db2c23c93f165c8de41
Chris Heppell <clheppell@icloud.com>
2020-08-04 22:00:47 +0100

    Handle marking DD disputes

--- controllers/stripe/dispute.js
+++ controllers/stripe/dispute.js
@@ -25,6 +25,7 @@ async function updateDetails(org, stripe, dispute) {
     }
   }
 
+  // Update dispute info
   var [results, fields] = await mysql.query("UPDATE `stripeDisputes` SET `Amount` = ?, `Currency` = ?, `PaymentIntent` = ?, `Reason` = ?, `Status` = ?, `Created` = ?, `EvidenceDueBy` = ?, `IsRefundable` = ?, `HasEvidence` = ?, `EvidencePastDue` = ?, `EvidenceSubmissionCount` = ? WHERE `SID` = ?", [ 
     dispute.amount,
     dispute.currency,
@@ -43,6 +44,15 @@ async function updateDetails(org, stripe, dispute) {
 
 exports.handleCreated = async function (org, stripe, dispute) {
   // Handle new dispute
+
+  dispute = await stripe.disputes.retrieve(
+    dispute.id, {
+    expand: ['payment_intent.payment_method'],
+  },
+    {
+      stripeAccount: org.getStripeAccount()
+    }
+  );
 
   let created, dueBy = null;
 
@@ -81,21 +91,77 @@ exports.handleCreated = async function (org, stripe, dispute) {
 }
 
 exports.handleClosed = async function (org, stripe, dispute) {
+
+  dispute = await stripe.disputes.retrieve(
+    dispute.id, {
+    expand: ['payment_intent.payment_method'],
+  },
+    {
+      stripeAccount: org.getStripeAccount()
+    }
+  );
+
   // Handle closed dispute
   await updateDetails(org, stripe, dispute);
 }
 
 exports.handleFundsWithdrawn = async function (org, stripe, dispute) {
+
+  dispute = await stripe.disputes.retrieve(
+    dispute.id, {
+    expand: ['payment_intent.payment_method'],
+  },
+    {
+      stripeAccount: org.getStripeAccount()
+    }
+  );
+
   // Handle dispute - funds withdrawn
   await updateDetails(org, stripe, dispute);
+
+  // If BACS Direct Debit update payment record
+  if (dispute.payment_intent.payment_method.type && dispute.payment_intent.payment_method.type == 'bacs_debit') {
+    // Set matching payment as failed
+    let status = 'failed';
+
+    if (dispute.reason = 'credit_not_processed' || dispute.reason == 'duplicate' || dispute.reason == 'fraudulent' || dispute.reason == 'general' || dispute.reason == 'product_not_received' || dispute.reason == 'product_unacceptable' || dispute.reason == 'subscription_canceled' || dispute.reason == 'unrecognized') {
+      status = 'charged_back';
+    }
+
+    var [results, fields] = await mysql.query("UPDATE `payments` SET `Status` = ?, `stripeFailureCode` = ? WHERE `stripePaymentIntent` = ?", [
+      status,
+      dispute.reason,
+      dispute.payment_intent.id,
+    ]);
+  }
 }
 
 exports.handleFundsReinstated= async function (org, stripe, dispute) {
+
+  dispute = await stripe.disputes.retrieve(
+    dispute.id, {
+    expand: ['payment_intent.payment_method'],
+  },
+    {
+      stripeAccount: org.getStripeAccount()
+    }
+  );
+
   // Handle dispute - funds reinstated
   await updateDetails(org, stripe, dispute);
 }
 
 exports.handleUpdated = async function (org, stripe, dispute) {
+
+  dispute = await stripe.disputes.retrieve(
+    dispute.id, {
+    expand: ['payment_intent.payment_method'],
+  },
+    {
+      stripeAccount: org.getStripeAccount()
+    }
+  );
+
   // Handle dispute updates
   await updateDetails(org, stripe, dispute);
 }
