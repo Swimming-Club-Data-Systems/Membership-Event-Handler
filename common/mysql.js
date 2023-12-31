5efdd95e0d2834335c84218b8a874c63a52a7f98
Chris Heppell <chris.heppell@myswimmingclub.uk>
2023-12-31 00:07:10 +0000

    Update mysql.js

--- common/mysql.js
+++ common/mysql.js
@@ -13,7 +13,7 @@ async function createPool() {
   }
 
   pool = await mysql.createPool({
-    connectionLimit : 25,
+    connectionLimit : 5,
     host            : process.env.DB_HOST || 'localhost',
     user            : process.env.DB_USER,
     password        : process.env.DB_PASS,
